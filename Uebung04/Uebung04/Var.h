cc52b4d67b6a0fddf1586032d8005a0162a0f394
Paul Engelhardt <1paulEngelhardt@gmail.com>
2023-11-16 15:35:19 +0100

    added comments

--- Uebung04/Uebung04/Var.h
+++ Uebung04/Uebung04/Var.h
@@ -7,7 +7,11 @@
 class Var {
 
 public:
-
+	/// <summary>
+	/// vars need a reference to the used type
+	/// </summary>
+	/// <param name="name"></param>
+	/// <param name="type"></param>
 	Var(std::string name, std::shared_ptr<Type> type);
 	Var() = default;
 	virtual std::string ToString() = 0;
